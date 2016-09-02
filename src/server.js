57ddddde9dae68a77675c1d9e1874de3335d0630
tondaHack <t.hack@seznam.cz>
2016-09-02 11:42:02 +0200

    View engine

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const root =  path.resolve(__dirname);
+console.log(root);
+
+app.set('view engine', 'ejs');
+app.set('views',`${root}/views`);
+
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('pages/index');
 });
 
 app.listen(port, () => {
