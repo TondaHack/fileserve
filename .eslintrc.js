823d49eab2366dd2584b8b962218bd8815af251d
tondaHack <t.hack@seznam.cz>
2016-09-02 11:24:28 +0200

    Fileserve init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "indent": ["error", 4],
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
